cc564cdcb5064eaa29a2d9b67b7d6a5eedfe8f9e
AnitaNakeva <anita_nakevaa@abv.bg>
2025-07-28 15:35:15 +0300

    Added logout functionality to EasyData

---
+++ Pages/Admin/Logout.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UniCodeProject.API.Pages.Admin;
+
+public class LogoutModel : PageModel
+{
+    public async Task<IActionResult> OnPostAsync()
+    {
+        await HttpContext.SignOutAsync("AdminScheme");
+        return Redirect("/admin/login");
+    }
+}
---
+++ Pages/EasyData/EasyDataEntity.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UniCodeProject.API.Pages.EasyData;
+
+public class EasyDataEntity : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+    
+}
